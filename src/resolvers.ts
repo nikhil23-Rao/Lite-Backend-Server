833204d00731657b6d502af4e9b220b35f339a8f
Nikhil <nikhil23.rao@gmail.com>
2021-02-21 09:11:32 -0800

    Created OAuth Mutations.

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -48,6 +48,35 @@ export const resolvers = {
       // Returns JSONWebToken To Client
       return token;
     },
+    // OAuth Register Mutation
+    OAuthRegister: async (_: any, args: UserArgsInt) => {
+      await User.sync({ force: true });
+
+      // Check If User Is Already Registered
+      if (await User.findOne({ where: { email: args.email } })) {
+        throw new ApolloError("Account with the given email already exists.");
+      }
+
+      // Build The User With Args
+      const user = User.build({
+        username: args.username,
+        email: args.email,
+        id: "",
+      });
+
+      // Save User To PSQL Database
+      await user.save();
+
+      // Create JSONWebToken
+      const token = generateJWT({
+        username: user.username,
+        email: user.email,
+        id: user.id,
+      });
+
+      // Returns JSONWebToken To Client
+      return token;
+    },
     Login: async (_: any, args: UserArgsInt) => {
       // Check If User Email Is Real
       let user = await User.findOne({ where: { email: args.email } });
@@ -56,6 +85,19 @@ export const resolvers = {
       // If Email Is Real, Check If Password Is Valid
       const validPassword = await bcrypt.compare(args.password, user.password);
       if (!validPassword) return new ApolloError("Invalid email or password.");
+
+      // If Email And Password Is Valid Return JSONWebToken To Client
+      const token = generateJWT({
+        email: user.email,
+        id: user.id,
+        username: user.username,
+      });
+      return token;
+    },
+    OAuthLogin: async (_: any, args: UserArgsInt) => {
+      // Check If User Email Is Real
+      let user = await User.findOne({ where: { email: args.email } });
+      if (!user) return new ApolloError("Invalid google login.");
 
       // If Email And Password Is Valid Return JSONWebToken To Client
       const token = generateJWT({
